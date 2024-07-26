82c5e286823600865416ec27e99b2b8adce0d06b
vibegame <vibegame.official@gmail.com>
2024-07-26 22:34:21 +0300

    feat: add nextjs app and remove vite app

---
+++ packages/next-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Button } from "@core/ui";
+import { Router } from "@sdk/router";
+
+const router = new Router();
+
+export default function Home() {
+  return (
+    <div>
+      <Button>123</Button>
+    </div>
+  );
+}
